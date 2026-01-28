75d54ddb6285f95f2bab56c8416e789edcbc4237
H82M19\Student <Student>
2026-01-28 11:20:55 +0700

    them reactjs buoi 1

---
+++ Week3/my-app/src/Tuan01/components/ProductCard.jsx
@@ -0,0 +1,21 @@
+import anh from '../../assets/img/anh.jpg'
+import './ProductCard.css'
+
+function ProductCard() {
+    return (
+        <div className="product-card">
+            <div>
+                <img src={anh} alt="Sản phẩm" className="product-img" />
+            </div>
+            
+            <div className="product-info">
+                <h2 className="product-title">Tên sản phẩm</h2>
+                <p className="product-price">Giá: 20000</p>
+            </div>
+
+            <button className="product-btn">Add to cart</button>
+        </div>
+    )
+}
+
+export default ProductCard
